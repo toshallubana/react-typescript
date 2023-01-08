8f2400c1ce6815e476e352fe2f38ee03612708ba
toshallubana <toshal.t1@gmail.com>
2023-01-08 19:23:40 +0530

    typescript pratice

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -4,10 +4,21 @@ type CounterState = {
     count: number
 }
 
-type CounterAction = {
-    type: string
+// type CounterAction = {
+//     type: 'increment' | 'decrement' | 'reset'
+//     payload?: number | undefined
+// }
+
+type UpdateAction = {
+    type: 'increment' | 'decrement'
     payload: number
 }
+
+type ResetAction = {
+    type: 'reset'
+}
+
+type CounterAction = UpdateAction | ResetAction
 
 const initialState = { count: 0}
 
@@ -17,6 +28,8 @@ function reducer(state: CounterState, action: CounterAction) {
             return { count: state.count + action.payload}
         case 'decrement':
             return { count: state.count - action.payload}
+        case 'reset':
+            return initialState
         default: 
             return state
     }
@@ -33,6 +46,7 @@ const Counter = () => {
             <button onClick={() => dispatch({type: 'decrement', payload: 10})}>
                 Decrement 10
             </button>
+            <button onClick={() => dispatch({ type: 'reset' })}></button>
         </>
     )
 }
